9ae476b9ae23e0cd479a4bf6a8794505e8c4d2d0
niekuiqiang00278 <130385242+niekuiqiang00278@users.noreply.github.com>
2023-04-12 21:04:51 +0800

    cw
    cw

---
+++ data/base/red/PidRed.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/12 21:04
+# @Author  : cap669
+# @File    : PidRed.py
+# @Software: PyCharm
+from root.plux.XRed import XRed
+
+
+class PidRed(XRed):
+    def __init__(self):
+        XRed.__init__(self)
+        
--- root/plux/XRed.py
+++ root/plux/XRed.py
@@ -1,3 +1,29 @@
+import aioredis
+from aioredis import Redis, Connection
+def RedInjection(host: str, port: int, db: int, minsize: int, maxsize: int, encoding: str, decode_responses: bool):
+    def back(cls):
+        cls.host = host
+        cls.port = port
+        cls.db = db
+        cls.minsize = minsize
+        cls.maxsize = maxsize
+        cls.encoding = encoding
+        cls.decode_responses = decode_responses
+        return cls
+
+    return back
+
+
 class XRed:
     def __init__(self):
-        pass
+        self.pool:Redis
+    async def create_pool(self):
+        self.pool = await aioredis.from_url(
+            "redis://127.0.0.1", port=12008, db=0, encoding="utf-8", decode_responses=True,
+        )
+
+    async def close_pool(self):
+        if self.pool:
+            pass
+        else:
+            await self.pool.close()
